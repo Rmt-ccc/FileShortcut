2d59082bf6ee0608c7baf8aea4b15f9cea4f0daa
Rmt-ccc <tokecomy@proton.me>
2026-09-12 17:19:25 +0900

    Update ShortcutCreator.kt

--- app/src/main/java/com/nagopy/android/fileshortcut/ShortcutCreator.kt
+++ app/src/main/java/com/nagopy/android/fileshortcut/ShortcutCreator.kt
@@ -22,9 +22,9 @@ import android.content.Intent
 import android.content.pm.ShortcutManager
 import android.graphics.Bitmap
 import android.os.Build
-import android.support.v4.content.pm.ShortcutInfoCompat
-import android.support.v4.content.pm.ShortcutManagerCompat
-import android.support.v4.graphics.drawable.IconCompat
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
 import android.widget.Toast
 import timber.log.Timber
 
